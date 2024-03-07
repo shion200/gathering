7ca3f19244aebe6db87e60d25078a709bf03cf2c
shion200 <usushio2002@gmail.com>
2024-03-08 05:07:01 +0900

    Create back Button

--- front/src/components/Button.tsx
+++ front/src/components/Button.tsx
@@ -2,10 +2,19 @@ import styles from "./Button.module.css";
 import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 
-export const Button = () => {
+export const ToAl = () => {
    const navigate = useNavigate();
    const handleSubmit = () => {
         navigate("/alcohol");
     };
    return <button type = "button" className={styles.button} onClick = {handleSubmit}>お酒の追加</button>;
  };
+
+ export const ToHome = () => {
+   const navigate = useNavigate();
+   const handleSubmit = () => {
+        navigate("/");
+    };
+   return <button type = "button" className={styles.button} onClick = {handleSubmit}>戻る</button>;
+ };
+
--- front/src/pages/Alcohol.tsx
+++ front/src/pages/Alcohol.tsx
@@ -3,74 +3,75 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/Auth";
+import { ToHome } from "../components/Button";
 
 export const Alcohol = () => {
-  const [image, setImage] = useState<File>();
-  const { user } = useAuth();
-  const navigate = useNavigate();
+	const [image, setImage] = useState<File>();
+	const { user } = useAuth();
+	const navigate = useNavigate();
 
-  const handleImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (!files) {
-      return;
-    }
+	const handleImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const files = event.target.files;
+		if (!files) {
+			return;
+		}
 
-    // FileListのままだとforEachが使えないので配列に変換する
-    const fileArray = Array.from(files);
-    const file = fileArray[0];
-    setImage(file)
-  };
+		// FileListのままだとforEachが使えないので配列に変換する
+		const fileArray = Array.from(files);
+		const file = fileArray[0];
+		setImage(file);
+	};
 
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (!image) {
-      console.log("ファイルが選択されていません");
-      return
-    }
+	const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (!image) {
+			console.log("ファイルが選択されていません");
+			return;
+		}
 
-    if (!user) {
-      console.error("ログインしていません．")
-      return
-    }
+		if (!user) {
+			console.error("ログインしていません．");
+			return;
+		}
 
-    const storage = getStorage();
-    const storageRef = ref(storage, `/user/${user.uid}/${image.name}`);
+		const storage = getStorage();
+		const storageRef = ref(storage, `/user/${user.uid}/${image.name}`);
 
-    // アップロード処理
-    const snapshot = await uploadBytes(storageRef, image);
-    const downloadUrl = await getDownloadURL(snapshot.ref);
-    // API にPOSTする
-    const method = "POST";
+		// アップロード処理
+		const snapshot = await uploadBytes(storageRef, image);
+		const downloadUrl = await getDownloadURL(snapshot.ref);
+		// API にPOSTする
+		const method = "POST";
 
-    const idToken = await user.getIdToken()
-    await fetch('http://localhost:8787/alcohol', {
-      method,
-      headers: { 
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${idToken}`
-      },
-      body: JSON.stringify({ name: image.name, url: downloadUrl })
-    })
-      .then((response) => response.json())
-      // .then((responseJson) => {
-      //   console.log(responseJson);
-      // })
-      .catch((error) => {
-        console.error(error);
-      });
+		const idToken = await user.getIdToken();
+		await fetch("http://localhost:8787/alcohol", {
+			method,
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${idToken}`,
+			},
+			body: JSON.stringify({ name: image.name, url: downloadUrl }),
+		})
+			.then((response) => response.json())
+			// .then((responseJson) => {
+			//   console.log(responseJson);
+			// })
+			.catch((error) => {
+				console.error(error);
+			});
 
+		// Homeにリダイレクト
+		navigate("/");
+	};
 
-    // Homeにリダイレクト
-    navigate('/');
-  }
-
-  return (
-    <div className="App">
-      <h1>Form</h1>
-      <form onSubmit={onSubmit}>
-        <input type="file" onChange={handleImage} />
-        <button type="submit">Upload</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Form</h1>
+			<form onSubmit={onSubmit}>
+				<input type="file" onChange={handleImage} />
+				<button type="submit">Upload</button>
+				<ToHome />
+			</form>
+		</div>
+	);
 };
